dac2cd5ae6cac00d3e0f4472ce4446be8052ddab
TeeSeal <teesealz@gmail.com>
2018-02-18 17:45:47 +0200

    Add audio

--- src/Coub.js
+++ src/Coub.js
@@ -3,12 +3,13 @@ const Ffmpeg = require('fluent-ffmpeg')
 const { PassThrough } = require('stream')
 
 class Coub extends Ffmpeg {
-  constructor(stream, { width, height }) {
-    super(stream)
-    if (typeof stream === 'string') {
+  constructor(videoStream, audio, { width, height }) {
+    if (typeof videoStream === 'string') {
       throw new Error('Please use Coub.fetch() to create coubs from URLs.')
     }
 
+    super(videoStream)
+    this.audio = audio
     this.width = width
     this.height = height
   }
@@ -32,29 +33,42 @@ class Coub extends Ffmpeg {
     return this
   }
 
+  attachAudio() {
+    return this
+      .input(this.audio)
+      .addOption('-shortest')
+  }
+
   static async fetch(url, quality) {
     if (!['high', 'med'].includes(quality)) quality = 'high'
     const id = url.split('/').slice(-1)[0]
 
     const { data: metadata } = await axios
       .get(`http://coub.com/api/v2/coubs/${id}`)
-      .catch(() => null)
     if (!metadata) return null
 
-    const mp4 = metadata.file_versions.html5.video[quality].url
+    const {
+      video: {
+        [quality]: { url: videoURL }
+      },
+      audio: {
+        [quality]: { url: audioURL }
+      }
+    } = metadata.file_versions.html5
+
     const [width, height] = metadata.dimensions[
       quality === 'high' ? 'big' : 'med'
     ]
 
-    const { data: buffer } = await axios.get(mp4, {
+    const { data: videoBuffer } = await axios.get(videoURL, {
       responseType: 'arraybuffer'
     })
-    buffer[0] = buffer[1] = 0
+    videoBuffer[0] = videoBuffer[1] = 0
 
-    const stream = new PassThrough()
-    stream.end(buffer)
+    const videoStream = new PassThrough()
+    videoStream.end(videoBuffer)
 
-    return new Coub(stream, { width, height })
+    return new Coub(videoStream, audioURL, { width, height })
   }
 }
 
