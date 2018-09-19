5bc81e5a51980b98294c141e61d2576196146301
TeeSeal <teesealz@gmail.com>
2018-09-19 16:50:44 +0300

    Add Util class

---
+++ src/Util.js
@@ -0,0 +1,16 @@
+const { extname } = require('path')
+
+class Util {
+  constructor() {
+    throw new Error(`the ${this.constructor.name} may not be initialized.`)
+  }
+
+  static resolvePath(path = '', name, ext = 'mp4') {
+    if (!path) path = name
+    path = path.replace(/<name>/g, name)
+    if (!extname(path)) path += `.${ext}`
+    return path
+  }
+}
+
+module.exports = Util
