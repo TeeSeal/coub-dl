e45355971bc7f4f30dbf8ef459845a3b33abdc33
Alex Hincu <teeseal@sorta.moe>
2020-01-26 22:45:12 +0200

    Only re-encode audio

--- bin/coub-dl.js
+++ bin/coub-dl.js
@@ -24,10 +24,6 @@ const program = require('commander')
     '-d, --details',
     'use in order to view the logs from ffmpeg while it works'
   )
-  .option(
-    '-C, --copy-codec',
-    'copy the codecs from the input. Improves speed but might cause issues with sound in some players'
-  )
   .option('-f, --format <format>', 'output file format (mp4, gif etc.)')
 
 program.on('--help', () => {
@@ -46,7 +42,7 @@ program.parse(process.argv)
 
 // Main
 async function run() {
-  const { input, output, loop, audio, crop, scale, time, details, copyCodec, format } = program
+  const { input, output, loop, audio, crop, scale, time, details, format } = program
   if (!input) {
     return console.log('Please specify input. Use --help to see the list of options.')
   }
@@ -64,7 +60,7 @@ async function run() {
   if (scale) coub.scale(scale)
   if (time) coub.addOption('-t', time)
   if (details) coub.on('info', console.log)
-  if (!crop && !scale && copyCodec) coub.addOption('-c', 'copy')
+  if (!crop && !scale) coub.addOption('-c:v:0', 'copy')
   coub.addOption('-shortest')
 
   const path = resolvePath(output, coub.metadata.title, format)
